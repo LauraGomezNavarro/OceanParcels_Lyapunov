90d1a90d8265ffaa1ad92668d8c4ad53853def06
Laura Gomez Navarro <l.gomeznavarro@uu.nl>
2022-03-01 17:31:25 +0100

    Update FTLE_func_test.py
    added step and domain as input parameters and changed FTLE_f to FTLE_x (could be forward or backward)

--- code/FTLE_func_test.py
+++ code/FTLE_func_test.py
@@ -26,18 +26,19 @@ def dist_pairs_km(inlon1, inlon2, inlat1, inlat2):
 
     return distance
 
-def FTLE(filename, Td, savename):
+def FTLE(filename, Td, step, domain, savename):
     """
     filename: example '/data/oceanparcels/output_data/data_LauraGN/outputs_parcels/wtides/monthly/' + 'Particle_AZO_grid100000p_wtides_0601_hourly_MONTH.nc'
     Td = Simualtion length in days, for example 30. # days
+    step = initial separation of particles (in degrees).  Both in logitude and latitude. Example: step = .04 # degrees
+    domain = [minimum lon, maximum lon, minimum lat, maximum lat]; longitude in degrees East and latitude in degrees north. Example = [-35, -18, 30, 40]
     savename = example savename = savedir + 'FTLE_wtides_0601.npz' #'KDE_' + nfile.split('/')[-1].split('.nc')[0] + '.npz' ; savedir = '/data/oceanparcels/output_data/data_LauraGN/outputs_parcels/FTLE/'
     
     """
 
     ds = xr.open_dataset(filename)
 
-    step = .04 # degrees
-    grid_lons, grid_lats = np.meshgrid(np.arange(-35, -18+step, step), np.arange(30, 40+step, step))
+    grid_lons, grid_lats = np.meshgrid(np.arange(domain[0], domain[1]+step, step), np.arange(domain[2], domain[3]+step, step))
 
     x0 = np.reshape(ds['lon'][:,0].data, ( grid_lons.shape[0], grid_lons.shape[1] ))
     x1 = np.reshape(ds['lon'][:,-1].data,( grid_lons.shape[0], grid_lons.shape[1] ))
@@ -48,7 +49,7 @@ def FTLE(filename, Td, savename):
     H = x0.shape[0] 
     L = x0.shape[1]
 
-    FTLE_f = np.ones_like(x0) * np.nan
+    FTLE_x = np.ones_like(x0) * np.nan
 
     J = np.empty([2,2],float)
 
@@ -69,7 +70,7 @@ def FTLE(filename, Td, savename):
                 # its largest eigenvalue:
                 lamda = LA.eigvals(D)
                 lam_max = max(lamda)
-                FTLE_f[i][j] = (1/Td) * np.log(np.sqrt(lam_max))
+                FTLE_x[i][j] = (1/Td) * np.log(np.sqrt(lam_max))
                 
-    np.savez(savename, FTLE_f=FTLE_f) 
+    np.savez(savename, FTLE=FTLE_x) 
 
