676106b2caba7eee34ee0be30b8674782b376ce9
LauraGomezNavarro <l.gomeznavarro@uu.nl>
2024-01-23 18:09:17 +0100

    new example and functions

--- code/release_and_simu.py
+++ code/release_and_simu.py
@@ -11,48 +11,49 @@ from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 #from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter#
 import matplotlib.ticker as mticker
 
-####################
-# Parameters:
+def release_and_simu(grid_name, datetime_in, simu_length, domain, filename, outname):
+    """
+    grid_name: e.g. "1-64grid"
+    datetime_in: e.g. datetime(2019, 10, 1, 0, 0)
+    simu_length: in days e.g. 15.
+    domain: spatial domain of particle release e.g. domain = [360.-12, 360.-8, 32., 36.] #lomin, lomax, lamin, lamax
+    filename: e.g. '../validation/' + 'datos_Laura.nc'
+    outname: e.g. '../examples/' + "Particle_AZO_fFTLE_" + grid_name + "_" + str(nmonth).zfill(2) + str(start_day).zfill(2) + "_" + str(simu_length)[0:2] + "d"
+    """
+    ####################
+    # Defining grid of starting particles:
+    if grid_name == "1-8grid":
+        step = .125 # degrees
+    elif grid_name == "1-64grid":
+        step = 0.015625 # 1./64degrees
+    else:
+        print("Error")
+        fszfasgasg
 
-#Particle_AZO_grid100000_notides_Dec_week01_hourly.nc
-grid_name = "1-64grid"
-nyear = 2019
-nmonth = 10
-simu_length = 15. # monthly # days
-
-# Defining grid of starting particles:
-
-if grid_name == "1-8grid":
-    step = .125 # degrees
-elif grid_name == "1-64grid":
-    step = 0.015625 # 1./64degrees
-else:
-    print("Error")
-    fszfasgasg
+    lons, lats = np.meshgrid(np.arange(domain[0], domain[1]+step, step), np.arange(domain[2], domain[3]+step, step))
+    ####################
     
-lons, lats = np.meshgrid(np.arange(360.-30, 360.-15+step, step), np.arange(32, 38+step, step))
-lons.shape[0]*lons.shape[1]
+    ####################
+    # Parcels simulation
+    
+    variables = {'U': 'ugos', 'V': 'vgos'}
 
-data_path = '../validation/'
-fname = 'datos_Laura.nc'
+    filenames = {'U': filename, 'V': filename}
 
-outdir = '../examples/'
+    dimensions = {'U': {'lat': 'latitude', 'lon': 'longitude', 'time': 'time'},
+                  'V': {'lat': 'latitude', 'lon': 'longitude', 'time': 'time'}}
 
-# Define start date:
-start_day = 1 #[1, 8, 15, 22]
+    fieldset = FieldSet.from_netcdf(filenames, variables, dimensions)
 
-outname = outdir + "Particle_AZO_fFTLE_" + grid_name + "_" + str(nmonth).zfill(2) + str(start_day).zfill(2) + "_" + str(simu_length)[0:2] + "d"
-####################
+    
+    pset = ParticleSet(fieldset=fieldset, pclass=JITParticle, lon=lons, lat=lats, time=datetime_in) #30))
 
-####################
-# Parcels simulation
-pset = ParticleSet(fieldset=fieldset, pclass=JITParticle, lon=lons, lat=lats, time=datetime(nyear, nmonth, start_day, 0, 0)) #30))
+    output_file = pset.ParticleFile(name=outname, outputdt=timedelta(hours=6))
 
-output_file = pset.ParticleFile(name=outname, outputdt=timedelta(hours=6))
+    #minutes=6*60
+    pset.execute(AdvectionRK4, runtime=timedelta(days=simu_length), dt=timedelta(hours=6),
+          output_file=output_file) #recovery={ErrorCode.ErrorOutOfBounds: DeleteParticle},
 
-#minutes=6*60
-pset.execute(AdvectionRK4, runtime=timedelta(days=simu_length), dt=timedelta(hours=6),
-      output_file=output_file) #recovery={ErrorCode.ErrorOutOfBounds: DeleteParticle},
-
-output_file.export()  # export the trajectory data to a netcdf file
-####################
+    output_file.export()  # export the trajectory data to a netcdf file
+    
+    ####################
